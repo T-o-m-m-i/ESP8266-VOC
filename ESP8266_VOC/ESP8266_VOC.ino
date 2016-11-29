6b3a54627737d2da5f592f04759480af790a9e36
Tommi <jmat0@hotmail.com>
2016-11-29 23:08:52 +0200

    Running avg

--- ESP8266_VOC/ESP8266_VOC.ino
+++ ESP8266_VOC/ESP8266_VOC.ino
@@ -22,100 +22,107 @@ WiFiClient client;
 const unsigned long interval = 300000;  //5min  15min=900000
 unsigned long previousMillis = 300000;
 
+int16_t voc;
+float temp;
+float humid;
 
 void setup(void)
 {
-  Serial.begin(9600);
+	voc = 0;
+	temp = 0;
+	humid = 0;
 
-  //Connecting to SHT31
-  if (! sht31.begin(0x44)) {   //0x45 is an alternative address
-    Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
-  }
+	Serial.begin(9600);
 
-  Serial.println("Getting single-ended readings from AIN0..3");
-  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+	//Connecting to SHT31
+	if (! sht31.begin(0x44))  //0x45 is an alternative address
+	{
+		Serial.println("Couldn't find SHT31");
+		while (1) delay(1);
+	}
 
-  // The ADC input range (or gain) can be changed via the following
-  // functions, but be careful never to exceed VDD +0.3V max, or to
-  // exceed the upper and lower limits if you adjust the input range!
-  // Setting these values incorrectly may destroy your ADC!
-  //                                                                ADS1015  ADS1115
-  //                                                                -------  -------
-  ads.setGain(GAIN_TWOTHIRDS);  	// 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  // ads.setGain(GAIN_ONE);        	// 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  // ads.setGain(GAIN_TWO);        	// 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-  // ads.setGain(GAIN_FOUR);       	// 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-  // ads.setGain(GAIN_EIGHT);      	// 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-  // ads.setGain(GAIN_SIXTEEN);    	// 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-  ads.begin();
+	// The ADC input range (or gain) can be changed via the following
+	// functions, but be careful never to exceed VDD +0.3V max, or to
+	// exceed the upper and lower limits if you adjust the input range!
+	// Setting these values incorrectly may destroy your ADC!
+	//                                                                ADS1015  ADS1115
+	//                                                                -------  -------
+	ads.setGain(GAIN_TWOTHIRDS);  	// 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+	// ads.setGain(GAIN_ONE);        	// 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+	// ads.setGain(GAIN_TWO);        	// 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+	// ads.setGain(GAIN_FOUR);       	// 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+	// ads.setGain(GAIN_EIGHT);      	// 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+	// ads.setGain(GAIN_SIXTEEN);    	// 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+	ads.begin();
 
-  //Connecting to network
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+	//Connecting to network
+	Serial.println();
+	Serial.print("Connecting to ");
+	Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+	WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+	while (WiFi.status() != WL_CONNECTED) {
+	delay(500);
+	Serial.print(".");
+	}
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
 void loop(void)
 {
-  int16_t adc0;//, adc1, adc2, adc3;
+	/*  if (! isnan(t)) {  // check if 'is not a number'
+	//Serial.print("Temp *C = "); Serial.println(t);
+	} else {
+	Serial.println("Failed to read temperature");
+	}
 
-  adc0 = ads.readADC_SingleEnded(0);
-  Serial.print("AIN0: "); Serial.println(adc0);
+	if (! isnan(h)) {  // check if 'is not a number'
+	//Serial.print("Hum. % = "); Serial.println(h);
+	} else {
+	Serial.println("Failed to read humidity");
+	}*/
+	//Serial.println();
 
-  float t = sht31.readTemperature();
-  float h = sht31.readHumidity();
+	//Send to ThingSpeak
+	if ((unsigned long)(millis() - previousMillis) >= interval)
+	{
+		for(int i=0; i<10; i++)
+		{
+			voc = (voc + ads.readADC_SingleEnded(0)) / 2.0;
+			temp = (temp + sht31.readTemperature()) / 2.0;
+			humid = (humid + sht31.readHumidity()) / 2.0;
+			delay(50);
+			//Serial.print("voc_i = "); Serial.println(voc);
+		}
 
-  if (! isnan(t)) {  // check if 'is not a number'
-    Serial.print("Temp *C = "); Serial.println(t);
-  } else {
-    Serial.println("Failed to read temperature");
-  }
+		if (client.connect(server,80)) //   "184.106.153.149" or api.thingspeak.com
+		{
+			String postStr = apiKey;
+			postStr +="&field1=";
+			postStr += String(voc);
+			postStr +="&field2=";
+			postStr += String(temp);
+			postStr +="&field3=";
+			postStr += String(humid);
+			postStr += "\r\n\r\n";
 
-  if (! isnan(h)) {  // check if 'is not a number'
-    Serial.print("Hum. % = "); Serial.println(h);
-  } else {
-    Serial.println("Failed to read humidity");
-  }
-  Serial.println();
+			client.print("POST /update HTTP/1.1\n");
+			client.print("Host: api.thingspeak.com\n");
+			client.print("Connection: close\n");
+			client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+			client.print("Content-Type: application/x-www-form-urlencoded\n");
+			client.print("Content-Length: ");
+			client.print(postStr.length());
+			client.print("\n\n");
+			client.print(postStr);
+		}
+		client.stop();
 
-  //Send to ThingSpeak
-  if ((unsigned long)(millis() - previousMillis) >= interval)
-  {
-	  if (client.connect(server,80)) {  //   "184.106.153.149" or api.thingspeak.com
-		String postStr = apiKey;
-			   postStr +="&field1=";
-			   postStr += String(adc0);
-			   postStr +="&field2=";
-			   postStr += String(t);
-			   postStr +="&field3=";
-			   postStr += String(h);
-			   postStr += "\r\n\r\n";
-
-		 client.print("POST /update HTTP/1.1\n");
-		 client.print("Host: api.thingspeak.com\n");
-		 client.print("Connection: close\n");
-		 client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
-		 client.print("Content-Type: application/x-www-form-urlencoded\n");
-		 client.print("Content-Length: ");
-		 client.print(postStr.length());
-		 client.print("\n\n");
-		 client.print(postStr);
-	  }
-	  client.stop();
-
-  previousMillis = millis();
-  }
-  delay(2000);
+		previousMillis = millis();
+	}
 }
